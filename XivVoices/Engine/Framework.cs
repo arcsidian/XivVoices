f0bc9285c6e594430c882f37354a2c4c960e909b
Arcsidian <arcsidian@gmail.com>
2024-04-24 16:07:14 +0300

    Dummy

---
+++ XivVoices/Engine/Framework.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XivVoices.Engine
+{
+    public class Framework
+    {
+        public Queue<XivMessage> Queue { get; set; } = new Queue<XivMessage>();
+
+        public Framework()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        internal async Task Process(XivMessage xivMessage)
+        {
+        }
+
+
+        public async Task Run(string directoryPath, bool once = false)
+        {
+        }
+
+    }
+}
